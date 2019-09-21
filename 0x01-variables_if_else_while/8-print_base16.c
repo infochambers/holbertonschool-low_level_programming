f49f4a1388ad52d37a6caa71fa619d95899eb775
Sol MacIan <github@infochambers.com>
2019-09-20 20:28:28 -0700

    #7

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+
+	int loop;
+
+	for (loop = '0' ; loop <= '9' ; loop++)
+	{
+		putchar(loop);
+	}
+
+	for (loop = 'a' ; loop <= 'e' ; loop++)
+	{
+		putchar(loop);
+	}
+
+	putchar('\n');
+
+		return (0);
+	}
