a738d00f6daaaadef3956731f53d3face4d22972
Sol MacIan <github@infochambers.com>
2019-09-20 20:22:38 -0700

    #7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+
+	int loop;
+
+	for (loop = 'z' ; loop >= 'a' ; loop--)
+	{
+		putchar(loop);
+	}
+
+	putchar('\n');
+
+		return (0);
+	}
