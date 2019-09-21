21a7ba0345d1ef5f980c6632ad47a940544def3a
Sol MacIan <github@infochambers.com>
2019-09-20 20:14:15 -0700

    #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+
+	int loop;
+
+	for (loop = '0' ; loop <= '9' ; loop++)
+	{
+		putchar(loop);
+	}
+
+	putchar('\n');
+
+		return (0);
+	}
