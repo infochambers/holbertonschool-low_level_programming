5c7062497274dd9dd2d5c8f577a372cb9648260d
Sol MacIan <github@infochambers.com>
2019-09-26 13:28:45 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers --  a function that prints 10 times the
+ * numbers, from 0 to 14, followed by a new line.
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void more_numbers(void)
+{
+
+	int srebmun;
+
+	for (srebmun = 0 ; srebmun < 15 ; srebmun++)
+	{
+		_putchar(srebmun + '0');
+	}
+
+	_putchar('\n');
+
+
+}
