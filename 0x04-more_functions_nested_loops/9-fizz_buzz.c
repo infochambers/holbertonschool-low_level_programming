bc8729e686470fd4d835b8caf6fff6113d7b2d6d
Sol MacIan <github@infochambers.com>
2019-09-26 15:21:41 -0700

    8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+
+		if (!(i % 5 == 0 || i % 3 == 0))
+		{
+			printf("%d ", i);
+		}
+
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+		return (0);
+	}
