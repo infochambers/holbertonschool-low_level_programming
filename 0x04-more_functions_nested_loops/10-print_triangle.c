244ae450c1759e33224e0c7b73a97fc5c5f3c841
Sol MacIan <github@infochambers.com>
2019-09-26 20:20:21 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,24 +13,25 @@
 void print_triangle(int size)
 {
 
-	int sqhigh, sqwide, space, shape;
+	//~ int sqhigh, sqwide, space, shape;
+	int sqhigh, space, shape;
 
 	for (sqhigh = 0 ; sqhigh < size ; sqhigh++)
 	{
 		if (size > 0)
 		{
-			for (sqwide = 1 ; sqwide <= size ; sqwide++)
-			{
-				for (space = sqwide - 1 ; space > 0 ; space--)
-				{
-				_putchar('.');
-				}
+			//~ for (sqwide = 1 ; sqwide <= size ; sqwide++)
+			//~ {
+					for (space = size - 1 ; space > 0 ; space--)
+					{
+					_putchar('.');
+					}
 
-				for (shape = 1 ; shape > sqwide ; shape++)
-				{
-				_putchar('#');
-				}
-			}
+					for (shape = 1 ; shape < size ; shape++)
+					{
+					_putchar('#');
+					}
+			//~ }
 		}
 		_putchar('\n');
 	}
