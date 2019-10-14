b85cc63e5a6203b459a285b33628ec387b52741f
Sol MacIan <github@infochambers.com>
2019-10-13 22:23:01 -0700

    project #A/1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * @argc: incoming argument
+ * @argc: incoming argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+	{
+
+	char **argv __attribute__((unused));
+
+	printf("%d\n", argc);
+
+	return (0);
+
+	}
