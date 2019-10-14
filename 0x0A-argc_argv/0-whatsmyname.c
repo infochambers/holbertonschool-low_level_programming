fa2b0522fd58c8b23a46debdfcce08d3cf2dd908
Sol MacIan <github@infochambers.com>
2019-10-13 22:05:12 -0700

    project #A/0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * @argc: incoming argument
+ * @argv: incoming argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+	{
+
+	argc = 0;
+
+	printf("%s \n", argv[argc]);
+
+	return (0);
+
+	}
