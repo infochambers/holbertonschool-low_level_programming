4b9ac67d2f372792e863af1535f88ad8ff954c41
Sol MacIan <github@infochambers.com>
2019-10-15 21:04:41 -0700

    project #B/2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat -- a function that concatenates two strings.
+ *
+ * @str: incoming characters
+ * @gnitrs: new string
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	char *gnitrs;
+	int count = 0, count2, first;
+
+
+	for (count = 0, first = 0 ; s1[count] != '\0' ; count++, first++)
+	{}
+	for ( ; s2[count] != '\0' ; count++)
+	{}
+
+	gnitrs = malloc(sizeof(char) * count);
+
+	for (count2 = 0 ; count2 <= first ; count2++)
+	{
+		gnitrs[count2] = s1[count2];
+	}
+	
+		for (count2 = 0 ; count2 <= count ; count2++)
+	{
+
+		gnitrs[count2] = s2[count2];
+	}
+
+return (gnitrs);
+
+}
