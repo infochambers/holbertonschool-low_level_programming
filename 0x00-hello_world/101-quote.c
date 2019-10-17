0ec51a5a8ab2adadf50d8d0c73ac8ff37a4a6962
Sol MacIan <github@infochambers.com>
2019-10-16 20:46:05 -0700

    project #0/101 review

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 	{
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
 
-		return (0);
+		return (1);
 	}
