67dbdb57ea4166109c51cc240cee49bd8f1690f6
Sol MacIan <github@infochambers.com>
2019-10-21 14:52:49 -0700

    D/2 update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,9 +3,14 @@
 /**
  * main - Entry point
  *
+ * @argc: incoming parameter, unused
+ * @argv: incoming parameter
+ *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char ** argv)
+
+
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 
