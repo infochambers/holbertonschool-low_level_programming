1e852502e156c773b0423447cdb9c3f33f8c970f
Sol MacIan <github@infochambers.com>
2019-10-21 14:41:29 -0700

    D/2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char ** argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
