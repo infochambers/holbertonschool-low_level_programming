d2f4cd8f44f2a38da794c46a3bcdd2881472a52a
Sol MacIan <github@infochambers.com>
2019-10-23 19:12:25 -0700

    Starting #F

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * array_iterator --  a function that executes a function given
+ * as a parameter on each element of an array.
+ *
+ * @array: incoming parameter
+ * @size: incoming parameter
+ * @action: incoming parameter
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+
+
+
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * array_iterator --  a function that executes a function given
+ * as a parameter on each element of an array.
+ *
+ * @array: incoming parameter
+ * @size: incoming parameter
+ * @action: incoming parameter
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+
+
+
+}
